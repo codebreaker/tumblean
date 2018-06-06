633bc4ea75db8b6605660031e3e1a35d0e4d9bbd
jagadeeshwarank <jagadeeshwaran.k@gmail.com>
2018-06-06 08:39:09 +0530

    Basic login achieved with Loglr

---
+++ app/src/main/java/in/flashbulb/tumblean/Application.kt
@@ -0,0 +1,5 @@
+package `in`.flashbulb.tumblean
+
+/**
+ * Created by sruthi on 4/29/18.
+ */
---
+++ app/src/main/java/in/flashbulb/tumblean/util/Prefs.kt
@@ -0,0 +1,12 @@
+package `in`.flashbulb.tumblean.util
+
+import android.content.SharedPreferences
+
+/**
+ * Created by sruthi on 3/17/18.
+ */
+class PreferenceHelper{
+    var defaultPreference : SharedPreferences;
+
+
+}
